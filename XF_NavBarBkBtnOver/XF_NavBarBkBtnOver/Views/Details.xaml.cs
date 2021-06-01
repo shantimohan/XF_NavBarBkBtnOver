e9579c39b90576f9f3c9d31a186f147211773bb7
Shantimohan Elchuri <shantimohan@outlook.com>
2021-06-01 14:09:36 -0400

    Made changes to use NavigationPage.TitleView as suggested in https://docs.microsoft.com/en-us/answers/questions/401635/setting-toolbar-to-link-nav-bar-back-button-overri.html

--- XF_NavBarBkBtnOver/XF_NavBarBkBtnOver/Views/Details.xaml.cs
+++ XF_NavBarBkBtnOver/XF_NavBarBkBtnOver/Views/Details.xaml.cs
@@ -10,40 +10,30 @@ using Xamarin.Forms.Xaml;
 namespace XF_NavBarBkBtnOver.Views
 {
     [XamlCompilation(XamlCompilationOptions.Compile)]
-    public partial class Details : BackButtonCustomActionPage
+    public partial class Details : ContentPage
     {
         public Details()
         {
             InitializeComponent();
-
-            if (EnableBackButtonOverride)
-            {
-                this.CustomBackButtonAction = async () =>
-                {
-                    var result = await this.DisplayAlert(null,
-                        "Hey wait now! Are you sure you want to go back?",
-                        "Yes go back", "Nope");
-
-                    if (result)
-                        //TODO: Changes as per JarvanZhang-MSFT in
-                        //https://docs.microsoft.com/en-us/answers/questions/401635/setting-toolbar-to-link-nav-bar-back-button-overri.html
-                        //await Navigation.PopAsync(true);
-                        await Navigation.PopModalAsync();
-                };
-            }
         }
 
         //TODO: Changes as per JarvanZhang-MSFT in
         //https://docs.microsoft.com/en-us/answers/questions/401635/setting-toolbar-to-link-nav-bar-back-button-overri.html
         private void ImageButton_Clicked(object sender, EventArgs e)
         {
-            this.CustomBackButtonAction.Invoke();
+            CustomBackButtonAction();
+        }
+
+        private async void CustomBackButtonAction()
+        {
+            if (await DisplayAlert(null, "Hey wait now! Are you sure you want to go back?", "Yes go back", "Nope"))
+                await Navigation.PopModalAsync();
         }
 
         // This override handler is needed to cater to the hardware back button
         protected override bool OnBackButtonPressed()
         {
-            this.CustomBackButtonAction.Invoke();
+            CustomBackButtonAction();
             return true;
         }
     }
